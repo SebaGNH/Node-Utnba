febe3adda91b1146e4f86617298ebea892382f93
Sebastian Gonzalez <sgonzalez@vates.com>
2024-08-13 23:38:36 -0300

    se realizaron peticiones get, post, send

--- Clase_02_02/index05.js
+++ Clase_02_02/index05.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { moviesRouter } from './routes/movies.routes.js';
 
 const app = express();
-const PORT = process.env.PORT ?? 8000; // process.env.PORT puerto designado por el sistema al crear el proyecto
+const PORT = process.env.PORT ?? 8000;
 
 app.listen(PORT, (err)=>{
     err
--- Clase_02_02/routes/movies.routes.js
+++ Clase_02_02/routes/movies.routes.js
@@ -1,17 +1,37 @@
 import { Router } from 'express';
-
+// http://localhost:8000/api/movies vemos las peticioens en postman
 export const moviesRouter = Router();
 
-moviesRouter.get("/", (req, res) => res.send("List all Movies")); // "/api/movies es la ruta a la que llegué originalmente
-moviesRouter.get("/:id", (req, res) => res.send("List movie by id"));
+moviesRouter.get("/", (req, res) => {
+    res.send("List all Movies");
+});
+
+// serch
+moviesRouter.get("/s", (req, res) => {
+    res.send(`Find movie by name ${req.query.title}`);
+});//   http://localhost:8000/api/movies/s?title=matrix
+
+
+moviesRouter.get("/:id", (req, res) => {
+    res.send(`List movie by id ${req.params.id}`);
+});
+// req.params.id <-- extraemos el parámetro id
 
 moviesRouter.post("/", (req, res) => res.send("Create Movies"));
 
-moviesRouter.patch("/:id", (req, res) => res.send("Update Movies"));
+moviesRouter.patch("/:id", (req, res) => {
+    const id = req.params.id; // <-- otro modo para sacar el parámetro
+    res.send(`Update Movies by id ${id}`);
+});
 
-moviesRouter.delete("/:id", (req, res) => res.send("Delete all Movies"));
+moviesRouter.delete("/:id", (req, res) => {
+    const { id } = req.params;
+    res.send(`Delete all Movies ${id}`);
+});
 
-// http://localhost:8000/api/movies vemos las peticioens en postman
+
+
+
 
 
 
